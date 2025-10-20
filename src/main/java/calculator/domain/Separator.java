057dc1913c007432ddadd14b05bb22ecce0781f7
kkb <gimgyubin1225@gmail.com>
2025-10-20 19:25:32 +0900

    refactor(Separator) : separatorMap의 자료형을 Map -> Set으로 변경
    구분자의 존재 여부를 확인하기 위한 기능은 Set이 적합

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -1,22 +1,22 @@
 package calculator.domain;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class Separator {
 
-    private final Map<String, Boolean> separatorMap = new HashMap<>();
+    private final Set<String> separatorMap = new HashSet<>();
 
     public Separator(List<String> defaultSeparators) {
         defaultSeparators.forEach(this::addSeparator);
     }
 
     public void addSeparator(String separator) {
-        separatorMap.put(separator, true);
+        separatorMap.add(separator);
     }
 
     public boolean isSeparator(String separator) {
-        return separatorMap.containsKey(separator);
+        return separatorMap.contains(separator);
     }
 }
